111a9031d662ddbc438e49652542e8d4f8c28216
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2013-06-18 11:27:00 +0200

    tests: add test-string
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

---
+++ tests/core/test-string.c
@@ -0,0 +1,35 @@
+#include "ku-string.h"
+
+#include <assert.h>
+
+static void
+test_ku_string_null (void)
+{
+    KuString *s = ku_string_new (0);
+    assert (ku_string_is_empty (s));
+    ku_string_free (s);
+}
+
+static void
+test_ku_string_full (void)
+{
+    KuString *s = ku_string_new ("abc");
+    assert (!ku_string_is_empty (s));
+    assert (ku_string_read_char (s) == 'a');
+    assert (ku_string_read_char (s) == 'b');
+    assert (!ku_string_is_empty (s));
+    assert (ku_string_read_char (s) == 'c');
+    assert (ku_string_is_empty (s));
+    assert (ku_string_read_char (s) == '\0');
+    assert (ku_string_is_empty (s));
+    assert (ku_string_read_char (s) == '\0');
+    assert (ku_string_is_empty (s));
+    ku_string_free (s);
+}
+
+int
+main (int __attribute__((unused)) argc, char __attribute__((unused)) *argv[]) {
+    test_ku_string_null ();
+    test_ku_string_full ();
+    return 0;
+}
