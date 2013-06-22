26642becb52484255f0b15d3fd2c35379240b85a
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2013-06-22 23:53:10 +0200

    core: define separators API
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

---
+++ src/core/ku-separator.h
@@ -0,0 +1,57 @@
+/*
+ *      This file is part of ku.
+ *
+ *      Copyright 2013 Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
+ *
+ *      ku is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      ku is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with ku.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __KU_SEPARATOR_H__
+#define __KU_SEPARATOR_H__
+
+#ifdef KU_COMPILATION
+#include "config.h"
+#endif
+
+#include <stdbool.h>
+
+typedef enum {
+    SPACE,
+    NEWLINE,
+    TAB,
+    COLON,
+    SEMI_COLON,
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_CURLY,
+    RIGHT_CURLY,
+    LEFT_BRACKET,
+    RIGHT_BRACKET,
+    DOT,
+    COMMA,
+    MINUS,
+    PLUS,
+    TIMES,
+    DIVIDE,
+    SQUOTE,
+    DQUOTE,
+    BQUOTE,
+    /* The token is not a reserved keyword */
+    NONE
+} KuSeparator;
+
+KuSeparator ku_token_cstring_to_separator (const char *token);
+bool        ku_token_cstring_is_separator (const char *token);
+
+#endif /*__KU_SEPARATOR_H__*/
